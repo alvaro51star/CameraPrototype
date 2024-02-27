afd81c7a8f7c247803e82daf78b5ad79ffa8f3d1
elenarojore <elena.red.kings@gmail.com>
2024-02-27 20:13:13 +0100

    camara hace fotos
    no las guarda, ni carrete, ni nada adicional al vídeo

---
+++ CameraPrototype/Assets/_Scripts/Camera/PhotoCapture.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoCapture : MonoBehaviour
+{
+    [Header("Photo Taker")]
+    [SerializeField] private Image photoDisplayArea;
+    [SerializeField] private GameObject photoFrame;
+    [SerializeField] private GameObject cameraUI;
+
+    [Header("Flash Effect")]
+    [SerializeField] private GameObject cameraFlash;
+    [SerializeField] private float flashTime;
+
+    [Header("Photo Fader Effect")]
+    [SerializeField] private Animator fadingAnimation;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource cameraAudio;
+
+    private Texture2D screenCapture;
+    private bool viewingPhoto;
+
+    private void Start()
+    {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(!viewingPhoto)
+            {
+                StartCoroutine(CapturePhoto());
+            }
+            else
+            {
+                RemovePhoto();
+            }
+        }
+    }
+
+    private IEnumerator CapturePhoto()//reads screen into texture
+    {
+        cameraUI.SetActive(false);//QUITAR TODA UI, SINO SALE EN LA FOTO
+        viewingPhoto = true;
+        yield return new WaitForEndOfFrame(); //asi lo hara despues de renderizar todo
+
+        Rect regionToRead = new Rect(0,0, Screen.width, Screen.height);
+
+        screenCapture.ReadPixels(regionToRead, 0, 0, false);//take capture of the screen and save that into texture
+        screenCapture.Apply();
+        ShowPhoto();
+    }
+
+    private void ShowPhoto()//turns texture into sprite and puts it in UI
+    {
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0, 0, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100);
+        photoDisplayArea.sprite = photoSprite;
+
+        photoFrame.SetActive(true);
+        StartCoroutine(CameraFlashEffect());
+        fadingAnimation.Play("PhotoFade");
+    }
+
+    private IEnumerator CameraFlashEffect()
+    {
+        cameraAudio.Play();
+        cameraFlash.SetActive(true);
+        yield return new WaitForSeconds(flashTime);
+        cameraFlash.SetActive(false);
+
+    }
+
+    private void RemovePhoto()
+    {
+        viewingPhoto = false;
+        photoFrame.SetActive(false);
+        cameraUI.SetActive(true);
+    }
+}
