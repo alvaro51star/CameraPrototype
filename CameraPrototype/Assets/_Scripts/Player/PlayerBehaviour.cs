ee0bee1bc2bff7e45a20642000f6e55823c58a07
mirikiwi <CoolKukinator@gmail.com>
2024-04-16 22:23:19 +0100

    Arreglo de la interacción con coger y soltar la camara

--- CameraPrototype/Assets/_Scripts/Player/PlayerBehaviour.cs
+++ CameraPrototype/Assets/_Scripts/Player/PlayerBehaviour.cs
@@ -8,28 +8,71 @@ public class PlayerBehaviour : MonoBehaviour
     //Variables
     private bool m_canInteract = false;
     private InteractiveObject m_interactingObject;
-    private bool m_canTakePicture = false;
+    private bool m_hasCameraEquiped = false;
+
+    private void OnEnable()
+    {
+        EventManager.OnUsingCamera += OnUsingCamera;
+        EventManager.OnNotUsingCamera += OnNotUsingCamera;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnUsingCamera -= OnUsingCamera;
+        EventManager.OnNotUsingCamera -= OnNotUsingCamera;
+    }
+    
+    private void OnNotUsingCamera()
+    {
+        m_hasCameraEquiped = false;
+        if (m_interactingObject != null)
+        {
+            IsBesideInteractableObject();
+        }
+    }
+
+    private void OnUsingCamera()
+    {
+        m_hasCameraEquiped = true;
+        StopInteracting();
+    }
+
+    public bool GetCanTakePicture()
+    {
+        return m_hasCameraEquiped;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         m_interactingObject = other.GetComponent<InteractiveObject>();
-        if (m_interactingObject != null)
+        if (m_interactingObject != null && !m_hasCameraEquiped)
         {
-            m_canInteract = true;
-            if (!m_interactingObject.GetNeedsButton())
-            {
-                Interaction();
-            }
-            else
-            {
-                UIManager.instance.ShowInput(true);
-            }
+            IsBesideInteractableObject();
         }
     }
+
+
+    private void IsBesideInteractableObject()
+    {
+        m_canInteract = true;
+        if (!m_interactingObject.GetNeedsButton())
+        {
+            Interaction();
+        }
+        else
+        {
+            UIManager.instance.ShowInput(true);
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        m_canInteract = false;
+        StopInteracting();
         m_interactingObject = null;
+    }
+    private void StopInteracting()
+    {
+        m_canInteract = false;
         UIManager.instance.ShowInput(false);
     }
 
