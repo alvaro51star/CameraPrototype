1c6e0e051a2a5bc69141cf1ee44f143a9f98e40d
mirikiwi <CoolKukinator@gmail.com>
2024-04-17 22:03:21 +0100

    Audios + Sonorización Caja fuerte

--- CameraPrototype/Assets/_Scripts/InteractiveObjects/SpecificActions/Interaction_CajaFuerte.cs
+++ CameraPrototype/Assets/_Scripts/InteractiveObjects/SpecificActions/Interaction_CajaFuerte.cs
@@ -6,6 +6,11 @@ public class Interaction_CajaFuerte : AffectsIndirectly
 {
     //Variables 
     [SerializeField] private string m_correctCode;
+    [SerializeField] private AudioClip[] m_keySounds;
+    [SerializeField] private AudioClip m_wrongSound;
+    [SerializeField] private AudioClip m_rightSound;
+    [SerializeField] private AudioClip m_safeUnlockSound;
+    [SerializeField] private AudioClip m_safeOpen;
     private bool m_isOpen = false;
     private string m_actualCode = "";
     private bool m_hasOpened;
@@ -21,6 +26,7 @@ public class Interaction_CajaFuerte : AffectsIndirectly
             }
             else
             {
+                AudioManager.Instance.ReproduceSound(m_safeOpen);
                 ChangeActiveMode(m_targetObject, true);
                 m_hasOpened = true;
             }
@@ -29,27 +35,39 @@ public class Interaction_CajaFuerte : AffectsIndirectly
 
     public void AddActualCode(GameObject button)
     {
+        ReproduceKeySound();
         m_actualCode += button.name;
         UIManager.instance.ChangeCodeDisplay(m_actualCode);
         UIManager.instance.ShowLight(false, true);
     }
 
+    public void ReproduceKeySound()
+    {
+        AudioClip clip = m_keySounds[Random.Range(0, m_keySounds.Length)];
+        AudioManager.Instance.ReproduceSound(clip);
+    }
+
     public void DeleteActualCode()
     {
+        ReproduceKeySound();
         m_actualCode = "";
         UIManager.instance.ChangeCodeDisplay(m_actualCode);
     }
 
     public void CheckCode()
     {
+        ReproduceKeySound();
         if (m_actualCode == m_correctCode)
         {
             m_isOpen = true;
             UIManager.instance.ShowLight(true, false);
+            AudioManager.Instance.ReproduceSound(m_rightSound);
+            AudioManager.Instance.ReproduceSound(m_safeUnlockSound);
         }
         else
         {
             UIManager.instance.ShowLight(true, true);
+            AudioManager.Instance.ReproduceSound(m_wrongSound);
         }
     }
 }
