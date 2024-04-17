7fc786d33100676ae721469117b6df517c14da1b
elenarojore <elena.red.kings@gmail.com>
2024-04-17 21:05:21 +0200

    Update EndTrigger.cs
    para lo del portapapeles

--- CameraPrototype/Assets/_Scripts/EndTrigger.cs
+++ CameraPrototype/Assets/_Scripts/EndTrigger.cs
@@ -10,6 +10,7 @@ public class EndTrigger : MonoBehaviour
         if(other.GetComponent<PlayerMovement>())
         {            
             uIManager.ActivateEndMenu();
+            GameManager.Instance.CopyTimeToClipboard();
         }//
     }
 }
