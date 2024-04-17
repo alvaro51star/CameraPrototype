3ff72b4fada94a338c4e23141993c9a558dbaffa
mirikiwi <CoolKukinator@gmail.com>
2024-04-17 18:31:01 +0100

    Modificación de script carrete

--- CameraPrototype/Assets/_Scripts/InteractiveObjects/SpecificActions/Interaction_Carrete.cs
+++ CameraPrototype/Assets/_Scripts/InteractiveObjects/SpecificActions/Interaction_Carrete.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interaction_Carrete : InteractivePickUp
+public class Interaction_Carrete : AffectsIndirectly
 {
     //Variables
     [SerializeField] int m_carreteAñadido;
-    public override void Action(GameObject player)
+
+    protected override void FirstAction()
     {
         EventManager.OnAddRoll?.Invoke(m_carreteAñadido);
         UIManager.instance.ShowInput(false);
