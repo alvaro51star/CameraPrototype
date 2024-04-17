9acb35af5fc5176a561fbbe8b7c2d2712881b19a
alvaro51star <alvaro51star@hotmail.es>
2024-04-17 18:11:28 +0200

    Añadido Timer

--- CameraPrototype/Assets/_Scripts/Managers/GameManager.cs
+++ CameraPrototype/Assets/_Scripts/Managers/GameManager.cs
@@ -1,11 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
 
+    private float playtestTime = 0f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +33,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            playtestTime += Time.deltaTime;
+        }
+    }
 
+    public void CopyTimeToClipboard()
+    {
+        TimeSpan time = TimeSpan.FromSeconds(playtestTime);
+        string timeString = string.Format("{0:00}:{1:00}", time.Minutes, time.Seconds);
+        CopyToClipboard(timeString);
     }
 
     public void CopyToClipboard(string str)
@@ -25,7 +51,8 @@ public class GameManager : MonoBehaviour
         GUIUtility.systemCopyBuffer = str;
     }
 
-    private void OnApplicationQuit() {
-        
+    private void OnApplicationQuit()
+    {
+        CopyTimeToClipboard();
     }
 }
