3d3f6a3cae32cbe78d932c9798a6d66dd7cb390a
alvaro51star <alvaro51star@hotmail.es>
2024-04-24 20:47:51 +0200

    Bug de reseteo del nivel solucionado

---
+++ CameraPrototype/Assets/_Scripts/Managers/LevelManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    
+}
