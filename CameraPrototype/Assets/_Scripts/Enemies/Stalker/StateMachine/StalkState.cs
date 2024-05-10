f7b9512b85337e35587ccf307a10931301ae7ead
alvaro51star <alvaro51star@hotmail.es>
2024-05-10 21:26:45 +0200

    IA adaptada a la progresion del nivel

--- CameraPrototype/Assets/_Scripts/Enemies/Stalker/StateMachine/StalkState.cs
+++ CameraPrototype/Assets/_Scripts/Enemies/Stalker/StateMachine/StalkState.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
+
 public class StalkState : State
 {
     [SerializeField] private GameObject enemy;
@@ -10,6 +12,15 @@ public class StalkState : State
 
     public float currentTime;
     [SerializeField] private float timeBeforeChangingPoint = 5f;
+
+    [SerializeField] private float timeToCompleteStalk_Level0 = 20f;
+    [SerializeField] private float timeToCompleteStalk_Level1 = 10f;
+    [SerializeField] private float timeToCompleteStalk_Level2 = 5f;
+
+    private const float timeLevel0 = 20f;
+    private const float timeLevel1 = 10f;
+    private const float timeLevel2 = 5f;
+
 
     public override void Enter()
     {
@@ -19,8 +30,6 @@ public class StalkState : State
         enemy.GetComponent<NavMeshAgent>().destination = enemy.transform.position;
         enemy.GetComponent<NavMeshAgent>().isStopped = true;
 
-        
-
         currentTime = 0f;
         animator.Play("Idle");
         isComplete = false;
@@ -29,7 +38,12 @@ public class StalkState : State
         {
             TPToNextPosition();
         }
+
+        ResetTimer();
+        CalculateTimes();
     }
+
+
 
     public override void Exit()
     {
@@ -46,11 +60,37 @@ public class StalkState : State
     {
         currentTime += Time.deltaTime;
 
-        if (currentTime >= timeBeforeChangingPoint && !objectMesh.isVisible)
+        if (LevelManager.instance.intensityLevel == 0)
         {
-            TPToNextPosition();
-            ResetTimer();
+            if (currentTime >= timeToCompleteStalk_Level0 && !objectMesh.isVisible)
+            {
+                isComplete = true;
+            }
         }
+        else if (LevelManager.instance.intensityLevel == 1)
+        {
+            if (currentTime >= timeToCompleteStalk_Level1 && !objectMesh.isVisible)
+            {
+                isComplete = true;
+            }
+        }
+        else if (LevelManager.instance.intensityLevel == 2)
+        {
+            if (currentTime >= timeToCompleteStalk_Level2 && !objectMesh.isVisible)
+            {
+                isComplete = true;
+            }
+        }
+        else if (LevelManager.instance.intensityLevel == 3)
+        {
+            if (currentTime >= timeBeforeChangingPoint && !objectMesh.isVisible)
+            {
+                TPToNextPosition();
+                ResetTimer();
+            }
+        }
+
+
     }
 
     private void TPToNextPosition()
@@ -66,6 +106,13 @@ public class StalkState : State
     private void ResetTimer()
     {
         currentTime = 0;
+    }
+
+    private void CalculateTimes()
+    {
+        timeToCompleteStalk_Level0 = Random.Range(timeLevel0 - timeLevel0 * 0.25f, timeLevel0 + timeLevel0 * 0.25f);
+        timeToCompleteStalk_Level1 = Random.Range(timeLevel1 - timeLevel1 * 0.25f, timeLevel1 + timeLevel1 * 0.25f);
+        timeToCompleteStalk_Level2 = Random.Range(timeLevel2 - timeLevel2 * 0.25f, timeLevel2 + timeLevel2 * 0.25f);
     }
 
     public void SetUp(GameObject enemy, Renderer objectMesh, Animator animator, StalkerBehaviour stalkerBehaviour)
