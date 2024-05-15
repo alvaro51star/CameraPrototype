f93c3a8fa3f60ba74967e143654c32209c40164f
alvaro51star <alvaro51star@hotmail.es>
2024-05-15 23:16:25 +0200

    Cambio en el punto de vision al enemigo

--- CameraPrototype/Assets/_Scripts/Enemies/Stalker/StateMachine/PlayerCatchState.cs
+++ CameraPrototype/Assets/_Scripts/Enemies/Stalker/StateMachine/PlayerCatchState.cs
@@ -41,7 +41,7 @@ public class PlayerCatchState : State
     private IEnumerator CatchPlayer(GameObject player)
     {
         player.GetComponent<PlayerMovement>().m_canWalk = false;
-        player.transform.LookAt(enemy.transform);
+        player.transform.LookAt(enemy.GetComponent<StalkerBehaviour>().pointToLook);
         navMesh.isStopped = true;
         navMesh.velocity = Vector3.zero;
         enemy.transform.position = player.GetComponent<WatchEnemy>().enemyCatchTp.position;
