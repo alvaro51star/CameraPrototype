6fd12f7658711becd91ba192dbb21351978885fa
EHaoqin <espee.delgado@gmail.com>
2024-08-21 11:21:12 +0200

    Quitado AudioSource y cambiado el script CatSounds

--- CameraPrototype/Assets/_Scripts/InteractiveObjects/Cat/CatSounds.cs
+++ CameraPrototype/Assets/_Scripts/InteractiveObjects/Cat/CatSounds.cs
@@ -4,21 +4,6 @@ using UnityEngine;
 
 public class CatSounds : MonoBehaviour
 {
-    private void OnEnable()
-    {
-        EventManager.OnCatPetted += OnCatPetted;
-    }
-
-    private void OnDisable()
-    {
-        EventManager.OnCatPetted -= OnCatPetted;
-    }
-
-    private void OnCatPetted()
-    {
-        AudioManager.Instance.PlayOneShot(FMODEvents.instance.meow /*, this.transform.position*/);
-    }
-
     public void CatGrowl()
     {
         AudioManager.Instance.PlayOneShot(FMODEvents.instance.growl /*, this.transform.position*/);
