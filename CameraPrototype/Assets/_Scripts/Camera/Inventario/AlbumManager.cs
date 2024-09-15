154f6bd2e8cea02d119cd68816a94a5384900513
mirikiwi <CoolKukinator@gmail.com>
2024-09-16 00:47:56 +0100

    Pruebas del guardado de fotos en files

--- CameraPrototype/Assets/_Scripts/Camera/Inventario/AlbumManager.cs
+++ CameraPrototype/Assets/_Scripts/Camera/Inventario/AlbumManager.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows;
 using UnityEngine.UI;
+using TMPro;
+//using static UnityEngine.Windows.File;
 
 public class AlbumManager : MonoBehaviour
 {
     //Variables
     public static AlbumManager instance;
     private List<Sprite> m_photos = new List<Sprite>();
+    
+    private int m_numPhotosTaken = 1;
+    private string filePath;
+    
+    [SerializeField] TextMeshProUGUI texto;  
     
 
     private int index = 0;
@@ -25,6 +36,11 @@ public class AlbumManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        texto.text = Application.persistentDataPath;
+    }
+
     public void AddPhoto(Sprite newPhoto)
     {
         Texture2D texture = new Texture2D(newPhoto.texture.width, newPhoto.texture.height);
@@ -33,10 +49,21 @@ public class AlbumManager : MonoBehaviour
         texture.SetPixels(pixels);
         texture.Apply();
 
-        Sprite sprite =  Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
-                                           new Vector2(0.5f, 0.5f), 100);
-
-        m_photos.Add(sprite);
+        //Sprite sprite =  Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+        //                                   new Vector2(0.5f, 0.5f), 100);
+        //
+        //m_photos.Add(sprite);
+        
+        byte[] byteArray = texture.EncodeToPNG();
+        filePath = Application.persistentDataPath + "/Photo" + SceneManager.GetActiveScene().name + m_numPhotosTaken + ".png";
+        //filePath = Application.persistentDataPath+ "/Resources/" + "Fotos" + SceneManager.GetActiveScene().name + "/Photo" + m_numPhotosTaken + ".png";
+        print("filepath: " + filePath);
+        System.IO.File.WriteAllBytes(filePath, byteArray);
+        m_numPhotosTaken++;
+        //using (FileStream fs = new FileStream(filePath, FileMode.Create))
+        //{
+        //    fs.
+        //}
     }
 
     public List<Sprite>  GetTandaPhoto(int pageIndex, int maxPhotoPerPage)
