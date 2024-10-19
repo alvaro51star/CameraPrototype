8b2bbee5400a08d42449a3cdddac4740431dbbfa
mirikiwi <CoolKukinator@gmail.com>
2024-10-19 11:38:44 +0100

    Cambio V1 Player Movement

--- CameraPrototype/Assets/_Scripts/Player/PlayerMovement.cs
+++ CameraPrototype/Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,24 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
     //Variables
-    [SerializeField] public float m_speed, m_rotationSpeedX, m_rotationSpeedY;
-    private CharacterController m_charController;
-    private Vector2 m_movementInputValue, m_playerRotateInputValue;
-    private float m_cameraRotateInputVaue;
     [SerializeField] private float m_upDownRange;
-    [SerializeField] private Transform m_camera;
-    public bool m_canWalk = true;
+    [FormerlySerializedAs("m_camera")] [SerializeField] private Transform m_tf_camera;
+    private CharacterController m_cc;
+    private Vector2 m_v2_movementInputValue, m_v2_playerRotateInputValue;
+    private float m_cameraRotateInputVaue;
+    [FormerlySerializedAs("m_speed")] public float speed;
+    [FormerlySerializedAs("m_rotationSpeedX")] public float rotationSpeedX;
+    [FormerlySerializedAs("m_rotationSpeedY")] public float rotationSpeedY;
+    [FormerlySerializedAs("m_canWalk")] public bool m_isCanWalk = true;
 
-    //gravity
+        //gravity
     [SerializeField] private float m_gravity = 9.8f;
     [SerializeField] private float m_terminalVelocity = 10;
     [SerializeField] private float m_verticalVelocity;
 
 
+    #region Setters
+        public void SetMovementInputValue(Vector2 inputValue)
+        {
+            m_v2_movementInputValue = inputValue;
+        }
+        
+        public void SetCameraRotateInputValue(float cameraInputValue)
+        {
+            m_cameraRotateInputVaue = cameraInputValue;
+        }
+        
+        public void SetPlayerRotateInputValue(Vector2 playerInputValue)
+        {
+            m_v2_playerRotateInputValue = playerInputValue;
+        }
+    #endregion
+    
     private void OnEnable()
     {
         EventManager.OnTakingPhoto += CanWalkFalse;
@@ -37,67 +57,55 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        m_charController = GetComponent<CharacterController>();
+        m_cc = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
         ApplyGravity();
 
-        if (!m_canWalk) return;
+        if (!m_isCanWalk) return;
         Movement();
         Rotation();
     }
 
-    private void IsReading()
-    {
-        CanWalkFalse();
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = true;
-    }
-
-    private void StopReading()
-    {
-        CanWalkTrue();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    private void CanWalkFalse()
-    {
-        m_canWalk = false;
-    }
-
-    private void CanWalkTrue()
-    {
-        m_canWalk = true;
-    }
-
-    public void SetMovementInputValue(Vector2 inputValue)
-    {
-        m_movementInputValue = inputValue;
-    }
-
-    public void SetCameraRotateInputValue(float cameraInputValue)
-    {
-        m_cameraRotateInputVaue = cameraInputValue;
-    }
+    #region Event Methods
+        private void IsReading()
+        {
+            CanWalkFalse();
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+        
+        private void StopReading()
+        {
+            CanWalkTrue();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        
+        private void CanWalkFalse()
+        {
+            m_isCanWalk = false;
+        }
+        
+        private void CanWalkTrue()
+        {
+            m_isCanWalk = true;
+        }
+    #endregion
     
-    public void SetPlayerRotateInputValue(Vector2 playerInputValue)
-    {
-        m_playerRotateInputValue = playerInputValue;
-    }
-
+    //Custom
     private void Movement()
     {
-        Vector3 movement = transform.right * m_movementInputValue.x + transform.forward * m_movementInputValue.y;
+        Vector3 movement = transform.right * m_v2_movementInputValue.x + transform.forward * m_v2_movementInputValue.y;
 
         movement.Normalize();
 
         //boorrar luego
         Vector3 upVector = Vector3.up * (m_verticalVelocity * Time.deltaTime);
 
-        m_charController.Move(movement *(m_speed * Time.deltaTime) + upVector);
+        m_cc.Move(movement *(speed * Time.deltaTime) + upVector);
     }
     
     private void Rotation()
@@ -108,27 +116,27 @@ public class PlayerMovement : MonoBehaviour
 
     private void PlayerRotation()
     {
-        float playerRotation = m_playerRotateInputValue.x * m_rotationSpeedX * Time.deltaTime;
+        float playerRotation = m_v2_playerRotateInputValue.x * rotationSpeedX * Time.deltaTime;
         transform.Rotate(0, playerRotation, 0);
     }
 
     private void CameraRotation()
     {
-        Vector3 cameraRotation = new Vector3(-m_playerRotateInputValue.y , 0, 0) * (m_rotationSpeedY * Time.deltaTime);
-        m_camera.Rotate(cameraRotation);
+        Vector3 cameraRotation = new Vector3(-m_v2_playerRotateInputValue.y , 0, 0) * (rotationSpeedY * Time.deltaTime);
+        m_tf_camera.Rotate(cameraRotation);
 
-        float angleTransformation = (m_camera.localEulerAngles.x > 180)
-            ? m_camera.localEulerAngles.x - 360
-            : m_camera.localEulerAngles.x;
+        float angleTransformation = (m_tf_camera.localEulerAngles.x > 180)
+            ? m_tf_camera.localEulerAngles.x - 360
+            : m_tf_camera.localEulerAngles.x;
 
         cameraRotation = new Vector3(Mathf.Clamp(angleTransformation, -m_upDownRange, m_upDownRange), 0, 0);
 
-        m_camera.localEulerAngles = cameraRotation;
+        m_tf_camera.localEulerAngles = cameraRotation;
     }
 
     private void ApplyGravity()
     {
-        if (!m_charController.isGrounded)
+        if (!m_cc.isGrounded)
         {
             if (m_verticalVelocity <= -m_terminalVelocity)
             {
